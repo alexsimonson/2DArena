de39cf7129925a5b94398763703c184a7cbfb664
Alex Simonson <alexsimonson@users.noreply.github.com>
2018-10-27 23:13:39 -0400

    Insta kill bullets, most basic shooting possible and working

--- Assets/Scripts/bulletMovement.cs
+++ Assets/Scripts/bulletMovement.cs
@@ -19,4 +19,16 @@ public class bulletMovement : MonoBehaviour {
 
 		rb.transform.Translate(targetForward*10*Time.deltaTime);
 	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log("Bullet Collides with something");
+		if(col.tag=="Collider"){
+			Debug.Log("Bullet hit wall");
+			Destroy(gameObject);
+		}else if(col.tag=="Enemy"){
+			Debug.Log("Bullet hit enemy");
+			Destroy(col.gameObject);
+			Destroy(gameObject);
+		}
+	}
 }
