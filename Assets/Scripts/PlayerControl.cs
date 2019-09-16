103c7f87e9c9d3763c6b69bd45a29a8ac7903134
Alex Simonson <shaggy7569@gmail.com>
2019-09-16 00:49:12 -0400

    kind of janky arms, progressing though!

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -28,10 +28,14 @@ public class PlayerControl : MonoBehaviour
     public Sprite playerForward;
     public Sprite playerBackward;
     public GameObject player;
+    public GameObject playerLeftArm;
+    public GameObject playerRightArm;
     // Use this for initialization
     void Start()
     {
         player = this.gameObject;
+        playerLeftArm = GameObject.Find("LeftArm");
+        playerRightArm = GameObject.Find("RightArm");
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
         fist = new Fist();
@@ -53,34 +57,55 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    void spriteManager(){
-        // Debug.Log("THIS IS THE FLIP : " + this.player.GetComponent<SpriteRenderer>().flip.y);
+    void spriteManager()
+    {
         Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         diff.Normalize();
         float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         // Debug.Log("DIFF X: " + diff.x);
-        if(diff.x > 0){
+        if (diff.x > 0)
+        {
             // this would be on the right side of the screen
             Debug.Log("Mouse on the right half of screen");
-            this.player.GetComponent<SpriteRenderer>().flipX = false;            
-        }else{
+            this.player.GetComponent<SpriteRenderer>().flipX = false;
+            // this is for disabling the left arm and enabling the right arm
+            this.playerLeftArm.GetComponent<SpriteRenderer>().enabled = false;
+            this.playerRightArm.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
             // this would be on the left side of the screen
             Debug.Log("Mouse on the left half of screen");
             this.player.GetComponent<SpriteRenderer>().flipX = true;
+            // this is for disabling the right arm and enabling the left arm
+            this.playerLeftArm.GetComponent<SpriteRenderer>().enabled = true;
+            this.playerRightArm.GetComponent<SpriteRenderer>().enabled = false;
+
         }
 
-        if(rotationZ > 0){
+        if (rotationZ > 0)
+        {
             // this would be on top of the screen
             Debug.Log("Mouse on the top of the screen");
-            if(this.player.GetComponent<SpriteRenderer>().sprite.name=="DemonHunter-Default"){
-                this.player.GetComponent<SpriteRenderer>().sprite = playerBackward;   
+            if (this.player.GetComponent<SpriteRenderer>().sprite.name == "DemonHunter-Default")
+            {
+                this.player.GetComponent<SpriteRenderer>().sprite = playerBackward;
             }
-        }else{
+            // this is for flipping the arm when going top to bottom
+            this.playerLeftArm.GetComponent<SpriteRenderer>().flipX = true;
+            this.playerRightArm.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
             // this would be on the bottom of the screen
             Debug.Log("Mouse on the bottom of the screen");
-            if(this.player.GetComponent<SpriteRenderer>().sprite.name=="DemonHunter-Default Up"){
-                this.player.GetComponent<SpriteRenderer>().sprite = playerForward;   
+            if (this.player.GetComponent<SpriteRenderer>().sprite.name == "DemonHunter-Default Up")
+            {
+                this.player.GetComponent<SpriteRenderer>().sprite = playerForward;
             }
+            // this is for flipping the arm when going top to bottom
+            this.playerLeftArm.GetComponent<SpriteRenderer>().flipX = false;
+            this.playerRightArm.GetComponent<SpriteRenderer>().flipX = false;
         }
         // diff x is left and right
         // rotation z is up and down
