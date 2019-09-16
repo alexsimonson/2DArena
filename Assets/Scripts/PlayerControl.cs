ca88531a9d64564c24c45f221e94f3839e5ba13b
Alex Simonson <shaggy7569@gmail.com>
2019-09-15 23:25:27 -0400

    basic character sprites hooked up to rotate, weapon display slot changed.  Weapons are a bit broken now

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -25,9 +25,13 @@ public class PlayerControl : MonoBehaviour
     public bool hasControl = true;
     public bool player1 = true;
 
+    public Sprite playerForward;
+    public Sprite playerBackward;
+    public GameObject player;
     // Use this for initialization
     void Start()
     {
+        player = this.gameObject;
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
         fist = new Fist();
@@ -45,7 +49,43 @@ public class PlayerControl : MonoBehaviour
         {
             MoveDirection();
             LookRotation();
+            spriteManager();
         }
+    }
+
+    void spriteManager(){
+        // Debug.Log("THIS IS THE FLIP : " + this.player.GetComponent<SpriteRenderer>().flip.y);
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+        float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        // Debug.Log("DIFF X: " + diff.x);
+        if(diff.x > 0){
+            // this would be on the right side of the screen
+            Debug.Log("Mouse on the right half of screen");
+            this.player.GetComponent<SpriteRenderer>().flipX = false;            
+        }else{
+            // this would be on the left side of the screen
+            Debug.Log("Mouse on the left half of screen");
+            this.player.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if(rotationZ > 0){
+            // this would be on top of the screen
+            Debug.Log("Mouse on the top of the screen");
+            if(this.player.GetComponent<SpriteRenderer>().sprite.name=="DemonHunter-Default"){
+                this.player.GetComponent<SpriteRenderer>().sprite = playerBackward;   
+            }
+        }else{
+            // this would be on the bottom of the screen
+            Debug.Log("Mouse on the bottom of the screen");
+            if(this.player.GetComponent<SpriteRenderer>().sprite.name=="DemonHunter-Default Up"){
+                this.player.GetComponent<SpriteRenderer>().sprite = playerForward;   
+            }
+        }
+        // diff x is left and right
+        // rotation z is up and down
+        // transform.rotation = Quaternion.Euler(0f, 0f, rotationZ - 90);
+        Debug.Log("ROTATION Z: " + rotationZ);
     }
 
     void MoveDirection()
@@ -69,7 +109,7 @@ public class PlayerControl : MonoBehaviour
             Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             diff.Normalize();
             float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ - 90);
+            // transform.rotation = Quaternion.Euler(0f, 0f, rotationZ - 90);
         }
         else if (player1 == false)
         {
